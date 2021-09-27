9e196fd9a703b78cf54331399fd96d2c9fb0b7ce
Martinius Feragen <andrmf18@uia.no>
2021-09-27 21:44:52 +0200

    trying to implement task 1.3

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ def print_dataset_simple(dataset):
 
 def main():
 	dataset = fm.get_data()
+	#
+	# sorted_by_lat = sorts.mergesort(list(dataset))
+	# print(f"number of merges done: {sorts.mergesort_merge_count}")
+	# print(f"number of nodes visited: {sorts.mergesort_node_count}")
+	# sorts.reset_globals()
+	#
+	# print("\nShuffling list")
+	# shuffled_list = list(dataset)
+	# shuffle(shuffled_list)
+	#
+	# sorted_by_lat_shuffled = sorts.mergesort(shuffled_list)
+	# print(f"number of merges done: {sorts.mergesort_merge_count}")
+	# print(f"number of nodes visited: {sorts.mergesort_node_count}")
+	#
 
-	sorted_by_lat = sorts.mergesort(list(dataset))
-	print(f"number of merges done: {sorts.mergesort_merge_count}")
-	print(f"number of nodes visited: {sorts.mergesort_node_count}")
-	sorts.reset_globals()
-
-	print("\nShuffling list")
-	shuffled_list = list(dataset)
-	shuffle(shuffled_list)
-
-	sorted_by_lat_shuffled = sorts.mergesort(shuffled_list)
-	print(f"number of merges done: {sorts.mergesort_merge_count}")
-	print(f"number of nodes visited: {sorts.mergesort_node_count}")
-
-
+	sorts.mergesort_from_pos(dataset, 1, 1)
 
 
 
--- sorts.py
+++ sorts.py
@@ -1,12 +1,15 @@
+import math
 
 mergesort_merge_count = 0
 mergesort_node_count = 0
+
 
 def reset_globals():
 	global mergesort_merge_count
 	global mergesort_node_count
 	mergesort_merge_count = 0
 	mergesort_node_count = 0
+
 
 def mergesort(data: list[dict]) -> list[dict]:
 	""" Sorts worldcites dataset by latitudes with mergesort """
@@ -57,9 +60,106 @@ def mergesort(data: list[dict]) -> list[dict]:
 		return data
 
 
-def mergesort2():
-	""" Sorts wordlcities datasets latitudes and longitude with mergesort """
-	raise NotImplemented
+def mergesort_from_pos(dataset, lat, lng):
+	"""
+	Sorts wordlcities datasets from a given coordinate with mergesort
+	using haversine formula from: https://www.movable-type.co.uk/scripts/latlong.html
+	a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+	c = 2 ⋅ atan2( √a, √(1−a) )
+	d = R ⋅ c """
+
+	def calculateDistance(lat1, lng1):
+		def inner(lat2, lng2):
+			def toRad(x): return x * (math.pi / 180)
+
+			R = 6371e3  # earths radius in m
+			# phi1 = toRad(lat1)
+			# phi2 = toRad(lat2)
+			#
+			# delta_phi = toRad((lat2 - lat1))
+			# delta_lambda = toRad((lng2 - lng1))
+			#
+			# a = (math.sin(delta_phi / 2) * math.sin(delta_phi / 2) +
+			# 	 math.cos(phi1) * math.cos(phi2) +
+			# 	 math.sin(delta_lambda / 2) * math.sin(delta_lambda / 2))
+			# # c = 2 * math.atan2(math.sqrt(a), math.sqrt((1 - a)))
+			# c = 2 * math.asin(min(1, math.sqrt(a)))
+			# d = R * c
+
+
+			# φ1 = toRad(lat1)
+			# λ1 = toRad(lng2)
+			# φ2 = toRad(lat2)
+			# λ2 = toRad(lng2)
+			#
+			# Δφ = φ2 - φ1
+			# Δλ = λ2 - λ1
+			#
+			# a = math.sin(Δφ / 2) * math.sin(Δφ / 2) + math.cos(φ1) * math.cos(φ2) * math.sin(Δλ / 2) * math.sin(Δλ / 2)
+			# c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+			# d = R * c
+
+			# Spherical Law of Cosines
+			# φ1 = toRad(lat1)
+			# φ2 = toRad(lat2)
+			# Δλ = toRad((lng2 - lng1))
+			# d = math.acos(math.sin(φ1) * math.sin(φ2) + math.cos(φ1) * math.cos(φ2) * math.cos(Δλ)) * R
+
+			return d
+
+		return inner
+
+	def mergesort2(data):
+		global mergesort_merge_count
+		global mergesort_node_count
+		if len(data) > 1:
+			# Gets middle index as int
+			m = len(data) // 2
+
+			left = mergesort2(data[:m])
+			right = mergesort2(data[m:])
+
+			i = 0  # current index of left side
+			j = 0  # current index of right side
+			k = 0  # current index of list index
+
+			l_len = 0 if left is None else len(left)
+			r_len = 0 if right is None else len(right)
+
+			mergesort_node_count += 1
+			while i < l_len and j < r_len:
+				mergesort_merge_count += 1
+
+				if dist_from_x(left[i]["lat"], left[i]["lng"]) < dist_from_x(right[j]["lat"], right[j]["lng"]):
+					data[k] = left[i]
+					i += 1
+				else:
+					data[k] = right[j]
+					j += 1
+
+				k += 1
+
+			# Adds any remaining elements from left or right side to the and of the list
+			while i < l_len:
+				mergesort_merge_count += 1
+				data[k] = left[i]
+				i += 1
+				k += 1
+
+			while j < r_len:
+				mergesort_merge_count += 1
+				data[k] = right[j]
+				j += 1
+				k += 1
+
+			return data
+
+		else:
+			return data
+
+	dist_from_x = calculateDistance(35.6897, 139.6922)
+	print(dist_from_x(-6.2146, 106.8451))
+	# mergesort2(dataset)
 
 
 def quicksort():
