13feee3b4488f18b2eb297034035bdfa955ccf38
Martinius Feragen <andrmf18@uia.no>
2021-09-27 12:25:56 +0200

    added mergesort for lists

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import filemanager as fm
+import sorts
 
 
 def main():
-	fm.get_data()
+	# fm.get_data()
+
+	asd = [12, 11, 13, 5, 6, 7]
+	sorts.mergesort(asd)
+	print(asd)
 
 
 if __name__ == "__main__":
--- sorts.py
+++ sorts.py
@@ -1,7 +1,44 @@
 
-def mergesort():
+def mergesort(listy):
 	""" Sorts worldcites dataset by latitudes with mergesort """
-	raise NotImplemented
+	if len(listy) > 1:
+		# Gets middle index as int
+		m = len(listy)//2
+
+		left = listy[:m]
+		right = listy[m:]
+
+		# pass by reference
+		mergesort(left)
+		mergesort(right)
+
+		i = 0  # current index of left side
+		j = 0  # current index of right side
+		k = 0  # current index of list index
+
+		l_len = len(left)
+		r_len = len(right)
+
+		while i < l_len and j < r_len:
+			if left[i] < right[j]:
+				listy[k] = left[i]
+				i += 1
+			else:
+				listy[k] = right[j]
+				j += 1
+
+			k += 1
+
+		# Adds any remaining elements from left or right side to the and of the list
+		while i < l_len:
+			listy[k] = left[i]
+			i += 1
+			k += 1
+
+		while j < r_len:
+			listy[k] = right[j]
+			j += 1
+			k += 1
 
 
 def mergesort2():
