30daf5356553a48e45299c96d5cc7d188a30ee3d
Martinius Feragen <andrmf18@uia.no>
2021-09-28 15:24:37 +0200

    #1 made mergesort function more generic

--- main.py
+++ main.py
@@ -4,32 +4,28 @@ from random import shuffle
 import filemanager as fm
 import sorts
 
-def print_dataset_simple(dataset):
-	for d in dataset:
-		print(d["lat"])
 
 def main():
 	dataset = fm.get_data()
-	#
-	# sorted_by_lat = sorts.mergesort(list(dataset))
-	# print(f"number of merges done: {sorts.mergesort_merge_count}")
-	# print(f"number of nodes visited: {sorts.mergesort_node_count}")
-	# sorts.reset_globals()
-	#
-	# print("\nShuffling list")
-	# shuffled_list = list(dataset)
-	# shuffle(shuffled_list)
-	#
-	# sorted_by_lat_shuffled = sorts.mergesort(shuffled_list)
-	# print(f"number of merges done: {sorts.mergesort_merge_count}")
-	# print(f"number of nodes visited: {sorts.mergesort_node_count}")
-	#
 
+	sorted_by_lat = sorts.mergesort(list(dataset), "lat")
+	print(f"number of merges done: {sorts.mergesort_merge_count}")
+	print(f"number of nodes visited: {sorts.mergesort_node_count}")
+	sorts.reset_globals()
+
+	print("\nShuffling list")
+	shuffled_list = list(dataset)
+	shuffle(shuffled_list)
+
+	sorted_by_lat_shuffled = sorts.mergesort(shuffled_list, "lat")
+	print(f"number of merges done: {sorts.mergesort_merge_count}")
+	print(f"number of nodes visited: {sorts.mergesort_node_count}")
+	sorts.reset_globals()
+
+	print("\nSorting by distant")
 	blarg = sorts.mergesort_from_pos(dataset, 1, 1)
-
-	for b in blarg:
-		print(b)
-
+	print(f"number of merges done: {sorts.mergesort_merge_count}")
+	print(f"number of nodes visited: {sorts.mergesort_node_count}")
 
 
 if __name__ == "__main__":
--- sorts.py
+++ sorts.py
@@ -11,16 +11,15 @@ def reset_globals():
 	mergesort_node_count = 0
 
 
-def mergesort(data: list[dict]) -> list[dict]:
-	""" Sorts worldcites dataset by latitudes with mergesort """
+def mergesort(data: list[dict], compare: str) -> list[dict]:
 	global mergesort_merge_count
 	global mergesort_node_count
 	if len(data) > 1:
 		# Gets middle index as int
 		m = len(data) // 2
 
-		left = mergesort(data[:m])
-		right = mergesort(data[m:])
+		left = mergesort(data[:m], compare)
+		right = mergesort(data[m:], compare)
 
 		i = 0  # current index of left side
 		j = 0  # current index of right side
@@ -32,7 +31,7 @@ def mergesort(data: list[dict]) -> list[dict]:
 		mergesort_node_count += 1
 		while i < l_len and j < r_len:
 			mergesort_merge_count += 1
-			if left[i]["lat"] < right[j]["lat"]:
+			if left[i][compare] < right[j][compare]:
 				data[k] = left[i]
 				i += 1
 			else:
@@ -68,7 +67,6 @@ def mergesort_from_pos(dataset, lat, lng):
 	c = 2 ⋅ atan2( √a, √(1−a) )
 	d = R ⋅ c
 	"""
-
 	def calculateDistance(lat1, lng1):
 		def inner(lat2, lng2):
 			R = 6371  # earths radius in m
@@ -76,7 +74,7 @@ def mergesort_from_pos(dataset, lat, lng):
 			lambda1 = math.radians(lng1)
 			phi2 = math.radians(lat2)
 			lambda2 = math.radians(lng2)
-			
+
 			delta_phi = phi2 - phi1
 			delta_lambda = lambda2 - lambda1
 
@@ -91,57 +89,9 @@ def mergesort_from_pos(dataset, lat, lng):
 		for d in data:
 			d["dist"] = dist_from(d["lat"], d["lng"])
 
-	def mergesort2(data):
-		global mergesort_merge_count
-		global mergesort_node_count
-		if len(data) > 1:
-			# Gets middle index as int
-			m = len(data) // 2
-
-			left = mergesort2(data[:m])
-			right = mergesort2(data[m:])
-
-			i = 0  # current index of left side
-			j = 0  # current index of right side
-			k = 0  # current index of list index
-
-			l_len = 0 if left is None else len(left)
-			r_len = 0 if right is None else len(right)
-
-			mergesort_node_count += 1
-			while i < l_len and j < r_len:
-				mergesort_merge_count += 1
-
-				if left[i]["dist"] < right[j]["dist"]:
-					data[k] = left[i]
-					i += 1
-				else:
-					data[k] = right[j]
-					j += 1
-
-				k += 1
-
-			# Adds any remaining elements from left or right side to the and of the list
-			while i < l_len:
-				mergesort_merge_count += 1
-				data[k] = left[i]
-				i += 1
-				k += 1
-
-			while j < r_len:
-				mergesort_merge_count += 1
-				data[k] = right[j]
-				j += 1
-				k += 1
-
-			return data
-
-		else:
-			return data
-
 	dist_from_x = calculateDistance(lat, lng)
 	add_dist_to_dataset(dataset, dist_from_x)
-	return mergesort2(dataset)
+	return mergesort(dataset, "dist")
 
 # mergesort2(dataset)
 
