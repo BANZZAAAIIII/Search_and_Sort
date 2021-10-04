63b74cb048ebba8f3db94a4c5bcd8e7a8603688b
Martinius Feragen <andrmf18@uia.no>
2021-10-04 21:03:34 +0200

    #4 added way to get freqancy of cities at lat, copied OBST

--- filemanager.py
+++ filemanager.py
@@ -1,12 +1,15 @@
 import csv
 
 
-def get_data() -> list[dict]:
+def get_data(norway=False) -> list[dict]:
 	dataset = []
 
 	with open("data/worldcities.csv", encoding='utf-8') as csv_file:
 		reader = csv.DictReader(csv_file)
 		for row in reader:
-			dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
+			if norway and row["country"] == "Norway":
+				dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
+			elif not norway:
+				dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
 
 	return dataset
--- main.py
+++ main.py
@@ -1,65 +1,80 @@
+from operator import itemgetter
+from itertools import groupby
 from random import shuffle
 
+import filemanager
 import filemanager as fm
 import search
 import sorts
 
 
 def main():
-	dataset = fm.get_data()
+	# dataset = fm.get_data()
 
-	print("1.1:")
-	sorted_by_lat = sorts.mergesort(list(dataset), "lat")
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
-	sorts.reset_globals()
+	# print("1.1:")
+	# sorted_by_lat = sorts.mergesort(list(dataset), "lat")
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	# sorts.reset_globals()
+	#
+	# print("\n1.2:")
+	# print("\tShuffling list")
+	# shuffled_list = list(dataset)
+	# shuffle(shuffled_list)
+	#
+	# sorted_by_lat_shuffled = sorts.mergesort(shuffled_list, "lat")
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	# sorts.reset_globals()
+	#
+	# print("\n1.3:")
+	# print("Sorting by distant")
+	# sorted_from_pos = sorts.mergesort_from_pos(list(dataset), 1, 1)
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	#
+	#
+	# print("\n2.1:")
+	# sorted_by_lat = sorts.quicksort(list(dataset), "lat")
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	# sorts.reset_globals()
+	#
+	#
+	# print("\n2.2:")
+	# print("\tShuffling list")
+	# shuffled_list = list(dataset)
+	# shuffle(shuffled_list)
+	#
+	# sorted_by_lat_shuffled = sorts.quicksort(shuffled_list, "lat")
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	# sorts.reset_globals()
+	#
+	#
+	# print("\n2.3:")
+	# print("Sorting by distant")
+	# sorted_from_pos = sorts.quicksort_from_pos(list(dataset), 1, 1)
+	# print(f"\tnumber of merges done: {sorts.merge_count}")
+	# print(f"\tnumber of nodes visited: {sorts.node_count}")
+	#
+	#
+	# print("\n3.1 search")
+	# print("\t" + str(search.binary_search(sorted_by_lat, "Catas Altas", -20.0750)))
+	# print("\t" + str(search.binary_search(sorted_by_lat, "Itaúna", -20.0750)))
 
-	print("\n1.2:")
-	print("\tShuffling list")
-	shuffled_list = list(dataset)
-	shuffle(shuffled_list)
+	dataset = filemanager.get_data()
 
-	sorted_by_lat_shuffled = sorts.mergesort(shuffled_list, "lat")
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
-	sorts.reset_globals()
+	counter = sorts.quicksort(dataset, "lat")
+	# for d in sorted(dataset, key=itemgetter("lat")):
+	# 	print(d)
 
-	print("\n1.3:")
-	print("Sorting by distant")
-	sorted_from_pos = sorts.mergesort_from_pos(list(dataset), 1, 1)
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
+	# results = groupby(counter, key=itemgetter("lat"))
+	# for key, valuesiter in results:
+	# 	print(key, [v["city"] for v in valuesiter])
 
 
-	print("\n2.1:")
-	sorted_by_lat = sorts.quicksort(list(dataset), "lat")
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
-	sorts.reset_globals()
-
-
-	print("\n2.2:")
-	print("\tShuffling list")
-	shuffled_list = list(dataset)
-	shuffle(shuffled_list)
-
-	sorted_by_lat_shuffled = sorts.quicksort(shuffled_list, "lat")
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
-	sorts.reset_globals()
-
-
-	print("\n2.3:")
-	print("Sorting by distant")
-	sorted_from_pos = sorts.quicksort_from_pos(list(dataset), 1, 1)
-	print(f"\tnumber of merges done: {sorts.merge_count}")
-	print(f"\tnumber of nodes visited: {sorts.node_count}")
-
-
-	print("\n3.1 search")
-	print("\t" + str(search.binary_search(sorted_by_lat, "Catas Altas", -20.0750)))
-	print("\t" + str(search.binary_search(sorted_by_lat, "Itaúna", -20.0750)))
-
+	search.OBST(dataset, 1, "a")
 
 if __name__ == "__main__":
 	main()
